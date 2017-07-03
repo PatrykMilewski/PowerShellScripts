7441e83ba9f1e3979b977bc0cbcad8b6b57eeaef
PatrykMilewski <patryk.milewski@gmail.com>
2017-07-03 10:26:16 +0200

    Importing MemoryLeak-Preventer script

---
+++ MemoryLeak-Prevent.ps1
@@ -0,0 +1,30 @@
+﻿[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$False, Position=1)]
+	 [Int]$sleepTime = 10,
+	[Parameter(Mandatory=$False, Position=2)]
+	 [Int]$maxMemorySize = 500,
+	[Parameter(Mandatory=$False)]
+	 [String]$sortBy = "WS",
+	[Parameter(Mandatory=$False)]
+	 [String]$memberName = "WorkingSet",
+	[Parameter(Mandatory=$False)]
+	 [Switch]$showPopUpWindows = $True
+)
+
+$wshell = New-Object -ComObject Wscript.Shell
+
+while($True) {
+	$process = get-process | Sort-Object -Descending $sortBy | select -first 1
+	$memoryUsage = $process.WS
+	$memoryUsage /= 1048576
+
+	if ($memoryUsage -gt $maxMemorySize) {
+		Stop-Process $process.Id
+		if ($showPopUpWindows -eq $True) {
+			$wshell.Popup(("Killed process " + $process.Name + " with ID: " + $process.Id),0,"Memory leak prevention",0x0)
+		}
+		echo ("Killed process " + $process.Name + " with ID: " + $process.Id)
+	}
+	Start-Sleep -Seconds $sleepTime
+}
