8b9500f0e94c50fdbf3497a255e17ef820c2fe7a
Patryk Milewski <patryk.milewski@gmail.com>
2017-07-05 10:49:17 +0200

    run script in background

---
+++ StartInBackground-Script.ps1
@@ -0,0 +1,8 @@
+﻿[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$False, Position=1)]
+	 [String]$scriptName = "VirtualMemoryLeak-Preventer.ps1"
+)
+
+$path = Join-Path $env:SystemDrive Scripts\$scriptName
+Start-Process PowerShell.exe -WindowStyle Hidden -ArgumentList "$path"
