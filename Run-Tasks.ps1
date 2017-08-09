10e1ba678175c8305eb1ffee413cf0b8fd6ba5c6
Patryk Milewski <patryk.milewski@gmail.com>
2017-08-09 22:53:17 +0200

    Working version 1.0
    Fixed remote execution of code. Added email notifcations sending feature. Fixed a few small bugs.

--- Run-Tasks.ps1
+++ Run-Tasks.ps1
@@ -1,10 +1,17 @@
-﻿
 function Run-Tasks {
     [CmdletBinding()]
     Param(
 	     [String]$IniFileLocation = (Join-Path $PSScriptRoot "Run-Tasks.ini"),
          [String]$LogFileName = "Run-Tasks.log",
-         [String]$LogFileLocation = (Join-Path $PSScriptRoot $LogFileName)
+         [String]$LogFileLocation = (Join-Path $PSScriptRoot $LogFileName),
+         [String]$TempFileDirectory = 'C:\Scripts\Temp\Run-Tasks',
+         [String]$emailFrom = "powiadomienia.madler@gmail.com",
+         [String]$emailTo = "patryk.milewski@gmail.com",
+         [String]$emailPassword,
+         [String]$emailSubject = "Run-Tasks Powershell script execution poblem.",
+         [String]$emailSmtp = "smtp.gmail.com",
+         [String]$emailPortsmtp = "587",
+         [Switch]$sendEmailNotifications = $False
     )
 
     BEGIN {
@@ -18,33 +25,42 @@ function Run-Tasks {
             [String]$arguments
             [Int]$runRemotly
             [String]$remoteTarget
-            [String]$stdout = (Join-Path $env:temp ("stdout" + [CustomProcess]::Counter + ".temp"))
-            [String]$stderr = (Join-Path $env:temp ("stderr" + [CustomProcess]::Counter + ".temp"))
+            [String]$stdout
+            [String]$stderr
             [String]$stdoutContent
             [String]$stderrContent
+            [String]$tempPath
             [Int]$errorCode
             [String]$errorText
             
-            CustomProcess([String]$exeFileLocation, [String]$arguments, [String]$runRemotly, [String]$remoteTarget) {
+            CustomProcess([String]$exeFileLocation, [String]$arguments, [String]$runRemotly, [String]$remoteTarget, [String]$tempPath) {
                 Write-Verbose ("Constructing CustomProcess Class instance number: " + [CustomProcess]::Counter + " with parameters 1: " + $exeFileLocation + " 2: " + $arguments + " 3: " + $runRemotly + " 4: " + $remoteTarget)
                 [CustomProcess]::Counter++
                 $this.exeFileLocation = $exeFileLocation
                 $this.arguments = $arguments
+                $this.remoteTarget = $remoteTarget
+                $this.tempPath = $tempPath
+
                 if ($runRemotly -like "true") {
                     $this.runRemotly = 1
                 }
                 else {
                     $this.runRemotly = 0
                 }
-                $this.remoteTarget = $remoteTarget
+
+                Execute-Command { param($path) if ((Test-Path $path) -eq $False)  { New-Item -ItemType Directory -Path $path -Force } } @($this.tempPath) $this.remoteTarget $this.runRemotly
+
+                $this.stdout = Join-Path $this.tempPath ("stdout" + [CustomProcess]::Counter + ".temp")
+                $this.stderr = Join-Path $this.tempPath ("stderr" + [CustomProcess]::Counter + ".temp")
             }
 
             StartProcess() {
                 Write-Verbose ("Starting process with exe: " + $this.exeFileLocation + " and arguments: " + $this.arguments)
-                [System.Management.Automation.ScriptBlock]$scriptBlock = { 
-                    Start-Process -NoNewWindow -FilePath $this.exeFileLocation -ArgumentList $this.arguments -RedirectStandardOutput $this.stdout -RedirectStandardError $this.stderr -ErrorAction Stop -Wait
+                [ScriptBlock]$scriptBlock = {
+                    param($exeFileLocation, $arguments, $stdout, $stderr)
+                    Start-Process -NoNewWindow -FilePath $exeFileLocation -ArgumentList $arguments -RedirectStandardOutput $stdout -RedirectStandardError $stderr -ErrorAction Stop -Wait
                 }
-                $this.errorCode = Execute-Command $scriptBlock $this.remoteTarget $this.runRemotly
+                $this.errorCode = Execute-Command $scriptBlock @($this.exeFileLocation, $this.arguments, $this.stdout, $this.stderr) $this.remoteTarget $this.runRemotly
                 if ($this.errorCode -ne 0) {
                     $this.errorText = $Error[0]
                 }
@@ -65,8 +81,8 @@ function Run-Tasks {
             }
 
             [bool]CheckStdout([String]$searchFor) {
-                if ((Execute-Command { Test-Path $this.stdout } $this.remoteTarget $this.runRemotly) -eq $True) {
-                    $this.stdoutContent = Execute-Command { Get-Content $this.stdout } $this.remoteTarget $this.runRemotly
+                if ((Execute-Command { param($stdout) Test-Path $stdout } @($this.stdout) $this.remoteTarget $this.runRemotly) -eq $True) {
+                    $this.stdoutContent = Execute-Command { param($stdout) Get-Content $stdout } @($this.stdout) $this.remoteTarget $this.runRemotly
                     Write-Verbose ("Standard output content: " + $this.stdoutContent)
                     if ($this.stdoutContent -like $searchFor) {
                         Write-Verbose ("Stndard output content matches search for parameter: " + $searchFor)
@@ -83,8 +99,8 @@ function Run-Tasks {
             }
 
             [bool]CheckStderr() {
-                if ((Execute-Command { Test-Path $this.stderr } $this.remoteTarget $this.runRemotly) -eq $True) {
-                    $this.stderrContent = Execute-Command { Get-Content $this.stderr } $this.remoteTarget $this.runRemotly
+                if ((Execute-Command { param($stderr) Test-Path $stderr } @($this.stderr) $this.remoteTarget $this.runRemotly) -eq $True) {
+                    $this.stderrContent = Execute-Command { param($stderr) Get-Content $stderr } @($this.stderr) $this.remoteTarget $this.runRemotly
                     if ($this.stderrContent[0].Length -gt 1) {
                         Write-Verbose ("Standard error content: " + $this.stderrContent)
                         return $True
@@ -100,25 +116,25 @@ function Run-Tasks {
             }
 
             Cleanup() {
-                if ((Execute-Command { Test-Path $this.stdout } $this.remoteTarget $this.runRemotly) -eq $True) {
+                if ($this.stdout -ne $null -and (Execute-Command { param($stdout) Test-Path $stdout } @($this.stdout) $this.remoteTarget $this.runRemotly) -eq $True) {
                     Write-Verbose ("Removing " + $this.stdout + " file")
-                    Execute-Command { Remove-Item $this.stdout } $this.remoteTarget $this.runRemotly
+                    Execute-Command { param($stdout) Remove-Item $stdout } @($this.stdout) $this.remoteTarget $this.runRemotly
                 }
-                if ((Execute-Command { Test-Path $this.stderr } $this.remoteTarget $this.runRemotly) -eq $True) {
+                if ($this.stderr -ne $null -and (Execute-Command { param($stderr) Test-Path $stderr } @($this.stderr) $this.remoteTarget $this.runRemotly) -eq $True) {
                     Write-Verbose ("Removing " + $this.stderr + " file")
-                    Execute-Command { Remove-Item $this.stderr } $this.remoteTarget $this.runRemotly
+                    Execute-Command { param($stderr) Remove-Item $stderr } @($this.stderr) $this.remoteTarget $this.runRemotly
                 }
             }
         }
 
-        function Execute-Command([ScriptBlock]$scriptBlock, [String]$remoteTarget, [Int]$runRemotly) {
+        function Execute-Command([ScriptBlock]$scriptBlock, [Object[]]$arguments, [String]$remoteTarget, [Int]$runRemotly) {
             if ($runRemotly -eq 1) {
                 Write-Verbose ("Executing remotly on: " + $remoteTarget + " script block: " + $scriptBlock)
-                return (Invoke-Command -ComputerName $remoteTarget -ScriptBlock $scriptBlock)                    
+                return (Invoke-Command -ComputerName $remoteTarget -ScriptBlock $scriptBlock -ArgumentList $arguments)                    
             }
             else {
                 Write-Verbose ("Executing locally script block: " + $scriptBlock)
-                return (& $scriptBlock)
+                return (Invoke-Command -ScriptBlock $scriptBlock -ArgumentList $arguments)
             }
         }
 
@@ -149,18 +165,56 @@ function Run-Tasks {
 	        }
         }
 
-        function Print-Paremeters([System.Array]$input) {
-            $log = ""
-            $counter = 1;
-            foreach ($parameter in $input) {
-                $log += (" " + $counter + ": " + $parameter)
+        Write-Verbose "Declaring EmailAutoresponder class"
+
+        Class EmailAutoresponder {
+            [String]$from
+            [String]$to
+            [pscredential]$credential
+            [String]$subject
+            [String]$smtp
+            [String]$port
+            [String]$body = ""
+            
+            EmailAutoresponder([String]$from, [String]$to, [String]$password, [String]$subject, [String]$smtp, [String]$port) {
+                Write-Verbose ("Constructing EmailAutoresponder instance with parameters 1: " + $from + " 2: " + $to + " 3: " + $password + " 4: " + $subject + " 5: " + $smtp + " 6: " + $port)
+                $this.from = $from
+                $this.to = $to
+                $secureString = ConvertTo-SecureString $password -AsPlainText -Force
+                $this.credential = New-Object System.Management.Automation.PSCredential ($this.from, $secureString)
+                $this.subject = $subject
+                $this.smtp = $smtp
+                $this.port = $port
             }
-            return $log
+
+            [bool]SendMail() {
+                try {
+                    Write-Verbose ("Sending email notification with body: " + $this.body)
+                    Send-MailMessage -From $this.from -To $this.to -Credential $this.credential -Subject $this.subject -Body $this.body -SmtpServer $this.smtp -Port $this.port -UseSsl -Encoding UTF8 -ErrorAction Stop
+                    Write-Verbose "Successfully sent notification"
+                    return $True
+                }
+                catch {
+                    Write-Verbose "Failed to send notification"
+                    return $False
+                }
+            }
+        }
+
+        function Print-Paremeters([String[]]$parameters) {
+            $log = ""
+            $counter = 1
+            foreach ($parameter in $parameters) {
+                $log += " " + $counter + ": " + $parameter
+                $counter++                
+            }
+            $log
         }
     }
 
     PROCESS {
-        [Logger]$logger = New-Object Logger($LogFileName, $LogFileLocation) 
+        [Logger]$logger = New-Object Logger($LogFileName, $LogFileLocation)
+        [EmailAutoresponder]$emailAutoresponder = New-Object EmailAutoresponder($emailFrom, $emailTo, $emailPassword, $emailSubject, $emailSmtp, $emailPortsmtp)
         Write-Verbose "Getting content from ini file"
         $iniFile = Get-Content $IniFileLocation
         Write-Verbose "Starting for each loop"
@@ -173,23 +227,43 @@ function Run-Tasks {
             $executeRemotly = $splittedLine[2]
             $executionTarget = $splittedLine[3]
             $stdoutMatch = $splittedLine[4]
+            $notifyOnNoMatch = $splittedLine[5]
 
-            [CustomProcess]$customProcess = New-Object CustomProcess($exeFileLocation, $arguments, $executeRemotly, $executionTarget)
+            [CustomProcess]$customProcess = New-Object CustomProcess($exeFileLocation, $arguments, $executeRemotly, $executionTarget, $TempFileDirectory)
             $customProcess.StartProcess()
+
             if ($customProcess.ErrorsFound() -eq $True) {
-                $logger.AddLog("Error code: " + $customProcess.errorCode + ", error text: " + $customProcess.errorCode)
+                $log = ("Process exited with error code: " + $customProcess.errorCode + ", error text: " + $customProcess.errorCode)
+                $emailAutoresponder.body += ($log + "`n")
+                $logger.AddLog($log)
             }
             if ($customProcess.CheckStderr() -eq $True) {
-                $logger.AddLog("Process with parameters" + (Print-Paremeters $splittedLine) + " exited with standard error: " + $customProcess.stderrContent)
+                $log = ("Process with parameters" + (Print-Paremeters $splittedLine) + " exited with standard error: " + $customProcess.stderrContent)
+                $emailAutoresponder.body  += ($log + "`n")
+                $logger.AddLog($log)
             }
-            if ($customProcess.CheckStdout($stdoutMatch)) {
-                $logger.AddLog("Process with parameters" + (Print-Paremeters $splittedLine) + " not matching expected standard output content. Expected: " + $stdoutMatch + " found: " + $customProcess.stdoutContent)
+            if ($customProcess.CheckStdout($stdoutMatch) -eq $False) {
+                $log = ("Process with parameters" + (Print-Paremeters $splittedLine) + " not matching expected standard output content. Expected: " + $stdoutMatch + " found: " + $customProcess.stdoutContent)
+                $emailAutoresponder.body  += ($log + "`n")
+                $logger.AddLog($log)
             }
+
             $customProcess.Cleanup()
         }
     }
 
     END {
+
+        if ($sendEmailNotifications -eq $True) {
+                $result = $emailAutoresponder.SendMail()
+            if ($result -eq $False) {
+                $logger.AddLog("Failed to send email notification with body: " + $emailAutoresponder.body)
+                $logger.AddLog("Error message: " + $Error[0])
+            }
+            else {
+                $logger.AddLog("Successfully sent notification with body: " + $emailAutoresponder.body)
+            }
+        }
         Write-Verbose "The end of script execution"
     }
 }
